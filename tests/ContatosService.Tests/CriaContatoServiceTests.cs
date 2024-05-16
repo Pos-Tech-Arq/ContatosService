14e450584d85b6bbc89d31f7244f03d5e1ba60dd
Natan Barros <natanbarros133@gmail.com.br>
2024-05-15 23:49:34 -0300

    add services in tests

--- tests/ContatosService.Tests/CriaContatoServiceTests.cs
+++ tests/ContatosService.Tests/CriaContatoServiceTests.cs
@@ -9,21 +9,22 @@ namespace ContatosService.Tests;
 
 public class CriaContatoServiceTests
 {
+    private Mock<IContatosRepository> _contatosRepository = new Mock<IContatosRepository>();
+    private Mock<IRegiaoRepository> _regiaoRepository = new Mock<IRegiaoRepository>();
+    private readonly Fixture _fixture = new Fixture();
+
     [Fact]
     public async Task Handle_ComDadosValidos_DeveCriarContatoComSucesso()
     {
-        var contatosRepository = new Mock<IContatosRepository>();
-        var regiaoRepository = new Mock<IRegiaoRepository>();
-        var fixture = new Fixture();
-        var command = fixture.Create<CriaContatoCommand>();
+        var command = _fixture.Create<CriaContatoCommand>();
         var cidades = new List<Cidade>();
-        cidades.Add(new Cidade(fixture.Create<string>()));
-        var regiao = new Regiao(command.Ddd, cidades, fixture.Create<string>());
-        regiaoRepository.Setup(c => c.Get(command.Ddd)).Returns(regiao);
-        var service = new CriaContatoService(contatosRepository.Object, regiaoRepository.Object);
+        cidades.Add(new Cidade(_fixture.Create<string>()));
+        var regiao = new Regiao(command.Ddd, cidades, _fixture.Create<string>());
+        _regiaoRepository.Setup(c => c.Get(command.Ddd)).Returns(regiao);
+        var service = new CriaContatoService(_contatosRepository.Object, _regiaoRepository.Object);
 
         await service.Handle(command);
 
-        contatosRepository.Verify(c => c.Create(It.IsAny<Contato>()), Times.Once);
+        _contatosRepository.Verify(c => c.Create(It.IsAny<Contato>()), Times.Once);
     }
 }
