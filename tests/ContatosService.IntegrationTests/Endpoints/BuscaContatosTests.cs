a3d832ec80f829b5a32e979e8706eb2ccc865a87
Milena Ramiro <milena.ramiro@fmd.ag>
2024-07-06 14:18:46 -0300

    test(BuscaContatosTests): coda teste busca contatos por ddd com sucesso

---
+++ tests/ContatosService.IntegrationTests/Endpoints/BuscaContatosTests.cs
@@ -0,0 +1,29 @@
+using System.Text.Json;
+using ContatosService.Domain.Entities;
+
+namespace ContatosService.IntegrationTests.Endpoints;
+
+public class BuscaContatosTests(ContatosServiceFactory factory) : Infra.IntegrationTests(factory)
+{
+    [Theory(DisplayName = "Busca contatos por DDD com sucesso")]
+    [InlineData("11")]
+    [InlineData("21")]
+    public async Task EditarContato_DeveRetornarComSucesso_QuandoParametrosValidos(string ddd)
+    {
+        // Arrange
+        ExecuteScript("insert_into_regioes_table.sql");
+        ExecuteScript("insert_into_cidades_table.sql");
+        ExecuteScript("insert_into_contatos_table.sql");
+
+        // Act
+        var response = await Client.GetAsync($"/api/v1/contatos?ddd={ddd}");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var content = await response.Content.ReadAsStringAsync();
+        var contatos = JsonSerializer.Deserialize<List<Contato>>(content);
+        contatos.Should().NotBeNullOrEmpty();
+        contatos.Should().HaveCount(3);
+        contatos.Should().Contain(c => c.Regiao.Ddd == ddd);
+    }
+}
