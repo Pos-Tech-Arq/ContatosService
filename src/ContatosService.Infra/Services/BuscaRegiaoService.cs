5039f07236ac6fd0b7480197ba1a0f029c87bfb6
Milena Ramiro <milenramiro@gmail.com>
2024-07-28 19:20:42 -0300

    chore(BuscaRegiaoService): trata erro quando ddd inválido

--- src/ContatosService.Infra/Services/BuscaRegiaoService.cs
+++ src/ContatosService.Infra/Services/BuscaRegiaoService.cs
@@ -1,6 +1,7 @@
 ﻿using ContatosService.Domain.Entities;
 using ContatosService.Infra.ExternalServices;
 using ContatosService.Infra.ExternalServices.BrasilApiService;
+using Refit;
 
 namespace ContatosService.Infra.Services;
 
@@ -15,10 +16,17 @@ public class BuscaRegiaoService : IBuscaRegiaoService
 
     public async Task<Regiao> BuscaRegiao(string ddd)
     {
-        var region = await _brasilApi.BuscaRegiaoPorDdd(ddd);
+        try
+        {
+            var region = await _brasilApi.BuscaRegiaoPorDdd(ddd);
 
-        var cidades = region.cities.Select(c => new Cidade(c)).ToList();
+            var cidades = region.cities.Select(c => new Cidade(c)).ToList();
 
-        return new Regiao(ddd, cidades, region.State);
+            return new Regiao(ddd, cidades, region.State);
+        }
+        catch (ApiException e)
+        {
+            throw new HttpRequestException("Código de região inválido");
+        }
     }
 }
